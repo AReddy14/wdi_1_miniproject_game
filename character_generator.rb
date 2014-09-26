7da0b0a701dde04fec62f3fe761ddcc89465a950
areddy14 <areddy1441@gmail.com>
2014-09-25 20:26:04 -0400

    I've sort of decided what to base my design off of. I've finished making a random stat distribution. I may change the numbers before I'm finished though.

---
+++ character_generator.rb
@@ -0,0 +1,39 @@
+#Creating a character generator. Probably going to just work based off of D&D or another rpg maybe ff tactics
+
+class Character
+	attr_accessor :hp, :str, :ma, :vit, :dex, :res, :mp
+	def initialize(hp=1,mp=0,str=0,ma=0,dex=0,vit=0,res=0)
+		@hp = hp
+		@mp = mp
+		@str = str
+		@ma = ma
+		@dex = dex
+		@vit = vit
+		@res = res
+	end
+
+	def random_stat_distribution
+		@str = 3+rand(1..10)
+		@vit = 3+rand(1..10)
+		@hp = (.5*@str + 2*@vit + 10).to_i
+		@ma = 3+rand(1..10)
+		@res = 3+rand(1..10)
+		@mp = (.5*@ma + 2*@res + 5).to_i
+		@dex = 3+rand(1..10)
+	end
+
+	def stat_dist_class_archtype(archtype)
+		if archtype == "wizard"
+			wizard_archtype
+		else
+			
+		end
+	end
+
+	def wizard_archtype
+		@str = 3+rand(1..5)
+		@vit = 3+rand(1..7)
+		@hp = (.5*str + 2*vit + 8)
+		
+	end
+end
