8cd8c2002391a297b15b98c852989f350a313ef4
areddy14 <areddy1441@gmail.com>
2014-09-27 21:14:43 -0400

    I've finished 4 out of the 5 classes I've planned.

--- character_generator.rb
+++ character_generator.rb
@@ -1,7 +1,8 @@
 #Creating a character generator. Probably going to just work based off of D&D or another rpg maybe ff tactics
-
+#The game will have to have at least two arrays as part of its design, one to save all PCs and one for NPCs
 class Character
-	attr_accessor :hp, :str, :ma, :vit, :dex, :res, :mp
+	#attr_accessor :hp, :str, :ma, :vit, :dex, :res, :mp
+	#I don't want them to be able to just set their stats according to their own whims
 	def initialize(hp=1,mp=0,str=0,ma=0,dex=0,vit=0,res=0)
 		@hp = hp
 		@mp = mp
@@ -59,4 +60,15 @@ class Character
 		@mp = (.5*ma + 2*res + 5)
 		@dex = 3+rand(5..15)
 	end
+
+	def cleric_archtype
+		@str = 3+rand(3..10)
+		@vit = 3+rand(1..8)
+		@hp = (.5*str + 2*vit + 8)
+		@ma = 3+rand(5..15)
+		@res = 3+rand(5..17)
+		@mp = (.5*ma + 2*res + 5)
+		@dex = 3+rand(1..10)
+	end
+
 end
